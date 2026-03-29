2482f631708bdd7783730ecc2b12795f0c505e9f
Dinali Ranasinghe <dinaliranasinghe2003@gmail.com>
2026-03-29 18:20:49 +0530

    Register Car model in admin, set up media files and superuser

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['title', 'brand', 'model', 'year', 'vehicle_type', 'starting_price', 'is_sold']
+    list_filter = ['vehicle_type', 'brand', 'is_sold', 'fuel_type']
+    search_fields = ['title', 'brand', 'model']
