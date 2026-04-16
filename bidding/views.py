172e024b97c111223566e0110570c2c86dafde98
thisalirj-star <thisalirj@gmail.com>
2026-04-17 01:23:02 +0530

    Add missing bidding_page view to fix routing error

--- bidding/views.py
+++ bidding/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def buyer_bidding_page(request, car_id):
     return render(request, 'bidding/bidding.html')
+
+def bidding_page(request):
+    return render(request, 'bidding/bidding.html')
