108a65175ef3ae8d61dd513cba35c9fa2e937cc9
thisalirj-star <thisalirj@gmail.com>
2026-04-17 13:50:23 +0530

    Update Bid model to include car_id

--- bidding/models.py
+++ bidding/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    car_id = models.IntegerField()  # temporary (until cars app connected)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
